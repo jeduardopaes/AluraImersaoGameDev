32730f85f8e7bc6cf21bb341a4bf13a95878d30f
José Eduardo Paes <j.eduardopaes@gmail.com>
2020-06-26 02:14:45 -0400

    Creating first enemy

---
+++ scripts/jogo/inimigo.js
@@ -0,0 +1,13 @@
+class Inimigo extends Animacao {
+
+
+    constructor(matriz, imagem, posicaoX,
+        largura, altura, larguraSprite, alturaSprite) {
+
+        super(matriz, imagem, posicaoX,
+            largura, altura, larguraSprite, alturaSprite);
+
+    }
+
+
+}
--- sketch.js
+++ sketch.js
@@ -1,13 +1,66 @@
 let imagemCenario;
 let imagemPersonagem;
+let imagemInimigo;
 let cenario;
 let personagem;
 let somDoJogo;
 
+const matrizPersonagem = [
+    [0, 0],
+    [220, 0],
+    [440, 0],
+    [660, 0],
+    [0, 270],
+    [220, 270],
+    [440, 270],
+    [660, 270],
+    [0, 540],
+    [220, 540],
+    [440, 540],
+    [660, 540],
+    [0, 810],
+    [220, 810],
+    [440, 810],
+    [660, 810],
+]
+
+const matrizInimigo = [
+    [0, 0],
+    [104, 0],
+    [208, 0],
+    [312, 0],
+    [0, 104],
+    [104, 104],
+    [208, 104],
+    [312, 104],
+    [0, 208],
+    [104, 208],
+    [208, 208],
+    [312, 208],
+    [0, 312],
+    [104, 312],
+    [208, 312],
+    [312, 312],
+    [0, 418],
+    [104, 418],
+    [208, 418],
+    [312, 418],
+    [0, 522],
+    [104, 522],
+    [208, 522],
+    [312, 522],
+    [0, 626],
+    [104, 626],
+    [208, 626],
+    [312, 626],
+]
+
 function preload() {
     imagemCenario = loadImage('assets/imagens/cenario/floresta.png');
     imagemPersonagem = loadImage('assets/imagens/personagem/correndo.png');
+    imagemInimigo = loadImage('assets/imagens/inimigos/gotinha.png');
     somDoJogo = loadSound('assets/sons/trilha_jogo.mp3');
+
 }
 
 // this function runs once
@@ -15,8 +68,10 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     cenario = new Cenario(imagemCenario, 5);
     personagem = new Personagem(imagemPersonagem, 220, 270);
+    inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 52, 52, 104, 104);
+
     somDoJogo.loop();
-    frameRate(24);
+    frameRate(32);
 }
 
 // this function updates canvas
@@ -24,5 +79,5 @@ function draw() {
     cenario.exibe();
     cenario.move();
     personagem.exibe();
-    personagem.anima();
+    inimigo.exibe();
 }
