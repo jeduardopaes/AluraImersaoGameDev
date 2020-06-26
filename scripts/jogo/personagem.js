cbfdd24df2fddf32e5c08218069449308d2bbee9
José Eduardo Paes <j.eduardopaes@gmail.com>
2020-06-26 02:13:42 -0400

    Fixing animation doll

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -9,15 +9,17 @@ class Personagem {
 
     exibe() {
         image(this.imagem, 25, height - 135, 110, 135, this.frame[0], this.frame[1], this.width, this.height);
+
+        this.anima();
     }
 
     anima() {
-        this.frame[0] += this.width + 1;
-        if (this.frame[0] > this.imagem.width) {
+        this.frame[0] += this.width;
+        if (this.frame[0] >= this.imagem.width) {
             this.frame[0] = 0;
-            this.frame[1] += this.height + 1;
+            this.frame[1] += this.height;
         }
-        if (this.frame[1] > this.imagem.height) {
+        if (this.frame[1] >= this.imagem.height) {
             this.frame[1] = 0;
         }
     }
